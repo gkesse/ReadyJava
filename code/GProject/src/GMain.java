4fdeb71920dce66c7aed0c02db3834cc10776f21
Gerard KESSE <tiakagerard@hotmail.com>
2020-08-20 08:17:08 +0200

    Initial Commit

---
+++ code/GProject/src/GMain.java
@@ -0,0 +1,7 @@
+//===============================================
+public class GMain {
+    public static void main(String[] args){
+        GProcess.Instance().run(args);
+    }
+}
+//===============================================
