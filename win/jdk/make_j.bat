d85b398dee0f0801b4251b3c9c5be7f16a395f70
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-13 22:41:34 +0100

    Initial Commit

--- win/jdk/make_j.bat
+++ win/jdk/make_j.bat
@@ -1,11 +1,10 @@
 @echo off
 ::===============================================
-set "args="             
+set "args=%*"             
 setlocal enabledelayedexpansion
 set "lArgs=%*"             
 set "lArgs=!lArgs:*%1 =!"  
 endlocal && ( set "args=%lArgs%" )
-echo %args%
 ::===============================================
-mingw32-make -f Makefile.jdk.mak %1 "argv=%args%"
+mingw32-make -f Makefile.jdk.mak %1 argv="%args%"
 ::===============================================
