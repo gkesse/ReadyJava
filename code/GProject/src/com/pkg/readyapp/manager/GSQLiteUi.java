97d93b6437db984135e4f6f9060d354fdfb32db5
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-14 03:11:40 +0100

    Initial Commit

--- code/GProject/src/com/pkg/readyapp/manager/GSQLiteUi.java
+++ code/GProject/src/com/pkg/readyapp/manager/GSQLiteUi.java
@@ -12,7 +12,7 @@ public class GSQLiteUi {
     }
     //===============================================
     public static synchronized GSQLiteUi Instance() {           
-        if(m_intance.equals(null) {   
+        if(m_intance == null) {   
             m_intance = new GSQLiteUi(); 
         }
         return m_intance;
@@ -21,28 +21,29 @@ public class GSQLiteUi {
     public void run(String[] args) {
         G_STATE = "S_INIT";
         while(true) {
-            if(G_STATE.equals("S_ADMIN") {run_ADMIN(args);}
-            else if(G_STATE.equals("S_INIT") {run_INIT(args);}
-            else if(G_STATE.equals("S_METHOD") {run_METHOD(args);}
-            else if(G_STATE.equals("S_CHOICE") {run_CHOICE(args);}
+            if(G_STATE.equals("S_ADMIN")) {run_ADMIN(args);}
+            else if(G_STATE.equals("S_INIT")) {run_INIT(args);}
+            else if(G_STATE.equals("S_METHOD")) {run_METHOD(args);}
+            else if(G_STATE.equals("S_CHOICE")) {run_CHOICE(args);}
             //
-            else if(G_STATE.equals("S_SHOW_TABLES") {run_SHOW_TABLES(args);}
+            else if(G_STATE.equals("S_SHOW_TABLES")) {run_SHOW_TABLES(args);}
             //
-            else if(G_STATE.equals("S_SAVE") {run_SAVE(args);}
-            else if(G_STATE.equals("S_LOAD") {run_LOAD(args);}
-            else if(G_STATE.equals("S_QUIT") {run_QUIT(args);}
+            else if(G_STATE.equals("S_SAVE")) {run_SAVE(args);}
+            else if(G_STATE.equals("S_LOAD")) {run_LOAD(args);}
+            else if(G_STATE.equals("S_QUIT")) {run_QUIT(args);}
             else break;
         }
     }        
     //===============================================
     public void run_ADMIN(String[] args) {
+        GProcessUi.Instance().run(args);
         G_STATE = "S_END";
     }
     //===============================================
     public void run_INIT(String[] args) {
         System.out.print(String.format("\n"));
         System.out.print(String.format("SQLITE_ADMIN !!!\n"));
-        System.out.print(String.format("\t%-2s : %s\n", "-q", "quitter l'application");
+        System.out.print(String.format("\t%-2s : %s\n", "-q", "quitter l'application"));
         System.out.print(String.format("\t%-2s : %s\n", "-i", "reinitialiser l'application"));
         System.out.print(String.format("\t%-2s : %s\n", "-a", "redemarrer l'application"));
         System.out.print(String.format("\t%-2s : %s\n", "-v", "valider les configurations"));        System.out.print(String.format("\n"));
@@ -83,9 +84,9 @@ public class GSQLiteUi {
     public void run_QUIT(String[] args) {
         System.out.print(String.format("\n"));
         String lAnswer = System.console().readLine(String.format("JAVA_QUIT (Oui/[N]on) ? : "));
-        if(lAnswer.equals("-q") G_STATE = "S_END";
-        else if(lAnswer.equals("-i")) G_STATE = "S_INIT";
-        else if(lAnswer.equals("-a")) G_STATE = "S_ADMIN";
+        if(lAnswer.equals("-q")) {G_STATE = "S_END";}
+        else if(lAnswer.equals("-i")) {G_STATE = "S_INIT";}
+        else if(lAnswer.equals("-a")) {G_STATE = "S_ADMIN";}
         else if(lAnswer.equals("o")) {G_STATE = "S_END";} 
         else if(lAnswer.equals("n")) {G_STATE = "S_INIT";} 
         else if(lAnswer.equals("")) {G_STATE = "S_INIT";}     
