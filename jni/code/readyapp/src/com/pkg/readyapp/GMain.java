f703d3630b7bf2148fda2afd7a83853be1417024
Gerard KESSE <tiakagerard@hotmail.com>
2020-12-21 12:30:07 +0100

    Initial Commit

--- jni/code/readyapp/src/com/pkg/readyapp/GMain.java
+++ jni/code/readyapp/src/com/pkg/readyapp/GMain.java
@@ -4,6 +4,13 @@ package com.pkg.readyapp;
 //import com.pkg.readyapp.manager.*;
 //===============================================
 public class GMain {
+    //===============================================
+    static {
+        System.loadLibrary("hello");
+    }
+    //===============================================
+    private native void sayHello();
+    //===============================================
     public static void main(String[] args){
         System.out.print(String.format("%s\n", "Bonjour tout le monde"));
     }
